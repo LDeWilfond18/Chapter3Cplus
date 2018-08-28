5456b7f3d72735459b685074ecbe124d026c459d
Logan DeWilfond <logan.dewilfond@gmail.com>
2018-08-28 11:09:20 -0500

    exercises

--- Mad Lib.cpp
+++ Mad Lib.cpp
@@ -18,33 +18,35 @@ int main()
 	cout << "Enter a color ";
 	cin >> color;
 
-	cout << "Enter a word ening in est ";
+	cout << "Enter a word ending in est ";
 	cin >> wordEst;
 
 	cout << "Enter a body part plural ";
 	cin >> bodyPartPlural;
 
-		cout << "Enter an animal";
+	cout << "Enter an animal ";
 	cin >> anAnimal;
 
-	cout << "Enter a Noun";
+	cout << "Enter a Noun ";
 	cin >> aNoun;
 
-	cout << "Enter a Plural Noun";
+	cout << "Enter a Plural Noun ";
 	cin >> pluralNoun;
 
 	cout << "Please enter a Number";
 	cin >> a;
 
-	cout << "Please enter another Number";
+	cout << "Please enter another Number ";
 	cin >> b;
 	
-	cout << "Please enter another Number";
+	cout << "Please enter another Number ";
 	cin >> c;
 
-	cout << "The " << color << "Dragon is the " << wordEst << "Dragon of all. It has " <<
-		c << bodyPartPlural << ", and a/an " << anAnimal << "shaped like a " <<
-		aNoun << ". It loves to eat " << pluralNoun << ", although it will feast on nearly anything.";
+	cout << "The " << color << " Dragon is the " << wordEst << " Dragon of all. It has \n" <<
+		c << " " << bodyPartPlural << ", and a/an " << anAnimal << " shaped like a \n" <<
+		aNoun << ". It loves to eat " << pluralNoun << ", although it will feast on nearly anything. ";
 
+	system("pause");
 
+	return 0;
 }
