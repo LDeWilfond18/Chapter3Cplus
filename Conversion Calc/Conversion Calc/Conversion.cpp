4e8aa645df09ac60837610a290464e49e09ec172
Logan DeWilfond <logan.dewilfond@gmail.com>
2018-08-29 10:22:49 -0500

    Conversion calc work

--- Conversion Calc/Conversion Calc/Conversion.cpp
+++ Conversion Calc/Conversion Calc/Conversion.cpp
@@ -14,12 +14,13 @@ int main()
 		cout << "Enter your value ";
 		cin >> firstNum;
 
-		pounds = firstNum / .6318;
-
-
+		pounds = firstNum * .6318;
+		pesos = firstNum * 12.8863;
+		yen = firstNum * 82.34;
 
 		cout << "There are " << pounds << " British Pounds in " << firstNum << " American dollars" << endl;
-		
+		cout << "There are " << pesos << " Mexican pesos in " << firstNum << " American dollars" << endl;
+		cout << "There are " << yen << " Japanese yen in " << firstNum << " American dollars" << endl;
 
 		system("pause");
 		return 0;
