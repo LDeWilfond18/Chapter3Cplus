978a9414d780393ecfcc3b5cc76331f9cab2d980
Logan DeWilfond <logan.dewilfond@gmail.com>
2018-08-30 11:10:29 -0500

    Hotel Stay work

---
+++ HotelStay/HotelStay/Hotel.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+string name;
+
+int main()
+{
+	double daysStayed = 0.0;
+	double roomCharge = 0.0;
+	double internetCharge = 0.0;
+	double totalBill = 0.0;
+
+	cout << "Enter the days stayed: ";
+	cin >> daysStayed;
+	cout << "Enter the room charge: ";
+	cin >> roomCharge;
+	cout << "Enter the internet charge: ";
+	cin >> internetCharge;
+	
+	totalBill = internetCharge * daysStayed + roomCharge;
+
+	cout << "*****************************\n";
+	cout << "Your total charge is: " << totalBill;
+	cout << "\n*****************************\n";
+
+	system("pause")
+		;
+	return 0;
+}
